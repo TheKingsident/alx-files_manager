58382dd76c53b0100ca8c3c0bf8e54363eda59a3
TheKingsident <kingsident@gmail.com>
2024-08-06 23:19:31 +0100

    Refactor import statements for redisClient and dbClient

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,29 +1,16 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-const getStatus = async (req, res) => {
-  try {
-    const isRedisAlive = await redisClient.isAlive();
-    const databaseStats = await dbClient.is
-    const isDBAlive = await dbClient.isAlive();
-
-    res.status(200).json({ redis: isRedisAlive, db: isDBAlive });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
-};
 
-const getStats = async (req, res) => {
-  try {
-    const nbUsers = await dbClient.nbUsers();
-    const nbFiles = await dbClient.nbFiles();
-
-    res.status(200).json({ users: nbUsers, files: nbFiles });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    res.status(200).json({ users: usersCount, files: filesCount });
   }
-};
+}
 
-module.exports = { getStatus, getStats };
+export default AppController;
