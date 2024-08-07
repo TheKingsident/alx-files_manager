7d8050db43e7001cb95100de7167d4414bd425b9
TheKingsident <kingsident@gmail.com>
2024-08-07 10:20:56 +0100

    Refactor import statements for unused controllers

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
-//import UsersController from '../controllers/UsersController';
-//import AuthController from '../controllers/AuthController';
-//import FilesController from '../controllers/FilesController';
+// import UsersController from '../controllers/UsersController';
+// import AuthController from '../controllers/AuthController';
+// import FilesController from '../controllers/FilesController';
 
 const express = require('express');
 
@@ -9,10 +9,10 @@ const AppController = require('../controllers/AppController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-//router.post('/users', UsersController.postNew);
-//router.get('/connect', AuthController.getConnect);
-//router.get('/disconnect', AuthController.getDisconnect);
-//router.get('/users/me', UsersController.getMe);
-//router.post('/files/', FilesController.postUpload);
+// router.post('/users', UsersController.postNew);
+// router.get('/connect', AuthController.getConnect);
+// router.get('/disconnect', AuthController.getDisconnect);
+// router.get('/users/me', UsersController.getMe);
+// router.post('/files/', FilesController.postUpload);
 
 module.exports = router;
