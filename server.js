e2a6c95264c3037d9c8e08f32ee83aecab9ec249
TheKingsident <kingsident@gmail.com>
2024-08-07 09:38:13 +0100

    feat: Add API routes for user management and file uploads

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const cors = require('cors');
-const dbClient = require('./utils/db');
-const redisClient = require('./utils/redis');
-const AppController = require('./controllers/AppController');
-const routes = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+const routes = require('./routes/index');
 
+app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
